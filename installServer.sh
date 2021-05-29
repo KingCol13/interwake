e684ac28c2784bcb80989bdf7d6b506cd2ceec99
KingCol13 <kingsleydgs@gmail.com>
2021-05-29 19:46:16 +0100

    File cleanup.

--- installServer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir /usr/local/etc/rpiwol
-cp mykey /usr/local/etc/rpiwol/mykey
-mkdir /usr/local/lib/systemd
-mkdir /usr/local/lib/systemd/system
-cp rpiwol.service /usr/local/lib/systemd/system/rpiwol.service
-
